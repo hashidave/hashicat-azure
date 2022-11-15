de08445455f5a98c60d5383338d3d30244791e57
HashiDaveR <106760442+hashidave@users.noreply.github.com>
2022-11-15 12:58:25 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. This is a pithy slogan. 
+  Welcome to ${PREFIX}'s app. Here's my super slogan. 
   <!-- END -->
   
   </div>
